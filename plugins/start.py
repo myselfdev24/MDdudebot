4045820525e9eb6a572c5d90fd77779a1ea1ff68
DevWilson24 <118274743+myselfdev24@users.noreply.github.com>
2023-02-20 10:29:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -65,10 +65,10 @@ async def cb_handler(client: Client , query: CallbackQuery):
              [[
                InlineKeyboardButton("♦️ Help", callback_data = "help")
              ],[
-               InlineKeyboardButton('📢 Update Channel', url='https://t.me/GreyMatter_Bots'),
-               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/GreyMatters_Bots_Discussion')
+               InlineKeyboardButton('📢 Update Channel', url='https://t.me/MD_Bots'),
+               InlineKeyboardButton('♻️ Suppot Group', url='https://t.me/MD_Support_group')
              ],[
-                InlineKeyboardButton('📃 Source code', url='https://github.com/Greymattersbot/md-renamebot')
+                InlineKeyboardButton('📃 Source code', url='https://github.com')
              ]]
         ))
         
